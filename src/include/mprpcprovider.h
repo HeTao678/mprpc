3e5832289553856b8d86dbaa45cc781baddd5ce8
HeTao678 <he1832418202@gmail.com>
2024-12-04 22:17:25 +0800

    provider的五个类写完了

--- src/include/mprpcprovider.h
+++ src/include/mprpcprovider.h
@@ -5,8 +5,9 @@
 #include <string>
 #include <functional>
 #include <iostream>
-#include <google/protobuf/service.h> // 添加这一行
-
+#include "google/protobuf/service.h" // 添加这一行
+#include <google/protobuf/descriptor.h>
+#include <unordered_map>
 
 // 框架提供的专门发布rpc服务的网络对象类
 class RpcProvider
@@ -29,4 +30,15 @@ private:
     void OnConnection(const muduo::net::TcpConnectionPtr &);
     // 如果muduo库发现有读写，就做 已经建立连接用户的读写事件回调
     void OnMessage(const muduo::net::TcpConnectionPtr &, muduo::net::Buffer *, muduo::Timestamp);
+    // Closure的回调操作，用于序列化rpc的响应和网络发送
+    void SendRpcResponse(const muduo::net::TcpConnectionPtr &, google::protobuf::Message *);
+
+    // service服务类型信息
+    struct ServiceInfo
+    {
+        google::protobuf::Service *m_service;                                                    // 保存服务对象
+        std::unordered_map<std::string, const google::protobuf::MethodDescriptor *> m_methodMap; // 保存服务方法
+    };
+    // 存储注册成功的服务对象和其服务方法的所有信息
+    std::unordered_map<std::string, ServiceInfo> m_serviceMap;
 };
